253c8aea9f70084beadfafd9a546d0f130dab602
DIGIPlus <admin@digiplus.cf>
2022-12-09 20:25:50 +0400

    ngOnChanges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { RoomsComponent } from './rooms/rooms.component';
 
 @Component({
   selector: 'digiplus-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
   title = 'hotelinventoryAPP';
   role = 'Admin';
+  
+  @ViewChild('user', { read: ViewContainerRef }) vcr!: ViewContainerRef;
+
+  ngAfterViewInit(): void {
+    const componentRef = this.vcr.createComponent(RoomsComponent);
+    componentRef.instance.numberOfRoom = 50;
+  }
 }
