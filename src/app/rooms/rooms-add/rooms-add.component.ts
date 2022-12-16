0c12b1b8b3ef80d6e9704dd6ee6d15464a6d5672
DIGIPlus <admin@digiplus.cf>
2022-12-16 20:34:41 +0400

    add forms

---
+++ src/app/rooms/rooms-add/rooms-add.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { RoomList } from '../rooms';
+import { RoomsService } from '../services/rooms.service';
+
+@Component({
+  selector: 'digiplus-app-rooms-add',
+  templateUrl: './rooms-add.component.html',
+  styleUrls: ['./rooms-add.component.scss']
+})
+export class RoomsAddComponent implements OnInit {
+  room: RoomList = {
+    roomType: '',
+    amenities: '',
+    checkinTime: new Date(),
+    checkoutTime: new Date(),
+    photos: '',
+    price: 0,
+    rating: 0,
+  }
+  successMessage: string = '';
+  constructor(private roomService: RoomsService) { }
+
+  ngOnInit(): void {}
+
+    addRoom() {
+      this.roomService
+      .addRoom(this.room)
+      .subscribe((data) =>
+      this.successMessage = (this.successMessage = 'Room Added'));
+
+    }
+  }
+
+
